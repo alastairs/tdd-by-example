c93f62d5c770934890658d39156d9e5aa1b9d234
Alastair Smith <alastair.smith@grantadesign.com>
2013-11-01 15:02:02 +0000

    First failing test: compilation failure.
    Use the best possible API for multiplication of the Dollar currency.
    
    Also update the to-do list with things we've added that we don't like.

---
+++ part-1/MultiCurrencyMoney/MultiCurrencyMoney.Tests/CurrencyShould.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace MultiCurrencyMoney.Tests
+{
+    public class CurrencyShould
+    {
+        [Fact]
+        public void ProvideTheCorrectAnswerToAMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.Equal(10, five.Amount);
+        }
+    }
+}
