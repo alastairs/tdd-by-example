4d3269835ab3754def85a2651cdf262e9b874e39
Alastair Smith <alastair.smith@grantadesign.com>
2013-11-01 15:04:55 +0000

    Fix the compilation errors.
    We now have a failing test.

--- part-1/MultiCurrencyMoney/MultiCurrencyMoney.Tests/CurrencyShould.cs
+++ part-1/MultiCurrencyMoney/MultiCurrencyMoney.Tests/CurrencyShould.cs
@@ -7,7 +7,7 @@ namespace MultiCurrencyMoney.Tests
         [Fact]
         public void ProvideTheCorrectAnswerToAMultiplication()
         {
-            Dollar five = new Dollar(5);
+            var five = new Dollar(5);
             five.Times(2);
             Assert.Equal(10, five.Amount);
         }
---
+++ part-1/MultiCurrencyMoney/MultiCurrencyMoney/Dollar.cs
@@ -0,0 +1,17 @@
+﻿namespace MultiCurrencyMoney
+{
+    public class Dollar
+    {
+        public Dollar(int i)
+        {
+            
+        }
+
+        public int Amount { get; set; }
+
+        public void Times(int i)
+        {
+            
+        }
+    }
+}
